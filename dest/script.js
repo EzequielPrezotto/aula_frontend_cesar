d487c6125838190204960ef61168430ace301ea2
luisfelipemisil <luisfelip80@gmail.com>
2023-07-24 00:19:38 -0300

    Add show list, rm & add task

--- dest/script.js
+++ dest/script.js
@@ -1,49 +1,38 @@
-interface Task {
-    id: number;
-    tarefa_texto: string;
-    completa: boolean;
-}
-
-let tarefas: Task[] = [];
+"use strict";
+let tarefas = [];
 let contador = 0;
-
 function addTask() {
-    const taskInput = document.getElementById("nova-tarefa") as HTMLInputElement;
+    const taskInput = document.getElementById("nova-tarefa");
     const tarefa_texto = taskInput.value.trim();
-    if (tarefa_texto === "") return;
-
-    const task: Task = {
+    if (tarefa_texto === "")
+        return;
+    const task = {
         id: contador++,
         tarefa_texto: tarefa_texto,
         completa: false,
     };
-
     tarefas.push(task);
     mostrarLista();
     taskInput.value = "";
 }
-
-function rmTask(taskId: number) {
+function rmTask(taskId) {
     tarefas = tarefas.filter((task) => task.id !== taskId);
     mostrarLista();
 }
-
-function toggleCompleted(taskId: number) {
+function toggleCompleted(taskId) {
     tarefas = tarefas.map((task) => {
         if (task.id === taskId) {
-            return { ...task, completa: !task.completa };
+            return Object.assign(Object.assign({}, task), { completa: !task.completa });
         }
         return task;
     });
     mostrarLista();
 }
-
 function mostrarLista() {
-    const tarefasIncompletas = document.getElementById("incompletas") as HTMLUListElement;
-    const tarefasCompletas = document.getElementById("completas") as HTMLUListElement;
+    const tarefasIncompletas = document.getElementById("incompletas");
+    const tarefasCompletas = document.getElementById("completas");
     tarefasIncompletas.innerHTML = "";
     tarefasCompletas.innerHTML = "";
-
     tarefas.forEach((task) => {
         const li = document.createElement("li");
         li.innerHTML = `
@@ -51,14 +40,12 @@ function mostrarLista() {
             <label>${task.tarefa_texto}</label>
             <button onclick="rmTask(${task.id})"  class="apagar">Apagar</button>
         `;
-        
-        if(task.completa){
+        if (task.completa) {
             tarefasCompletas.appendChild(li);
-        }else{
+        }
+        else {
             tarefasIncompletas.appendChild(li);
         }
-        
     });
 }
-
 mostrarLista();
