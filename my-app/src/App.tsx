4be7cdadc094c529312c381701ca53252c42a781
matheusberger <matheuscberger@gmail.com>
2023-07-27 15:36:51 -0300

    working list with completed and incompleted tasks

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,6 +12,16 @@ const App: React.FC = () => {
     setTaskName(event.target.value)
   };
 
+  const handleTaskStatus = (task: Task) => {
+    const index = taskList.indexOf(task)
+    const updatedTask = {
+      name: taskList[index].name,
+      completed: !taskList[index].completed
+    };
+    taskList.splice(index, 1)
+    setTaskList([...taskList, updatedTask])
+  };
+
   const addTask = (): void => {
     const newTask = {
       name: taskName,
@@ -21,16 +31,37 @@ const App: React.FC = () => {
     setTaskName("")
   }
 
+  function isNotCompleted(element: Task, index: number, array: Task[]) {
+    return (element.completed == false)
+  }
+
+  function isCompleted(element: Task, index: number, array: Task[]) {
+    console.log(element)
+    return (element.completed == true)
+  }
+
   return (
     <div className="App">
       <div className='header'>
-        <input type='text' value={taskName} placeholder='type your task here' onChange={handleChange}/>
-        <button onClick={addTask}>add</button>
+        <h1>Lista de tarefas</h1>
+        <div>
+          <input type='text' value={taskName} placeholder='type your task here' onChange={handleChange}/>
+          <button onClick={addTask}>add</button>
+        </div>
       </div>
       <div className='todoList'>
-        {taskList.map( (task: Task, key: number) => {
-          return <TaskItem key={key} task={task}/>
-        })}
+        <div className='pending'>
+          <h1>Pendentes</h1>
+          {taskList.filter(isNotCompleted).map( (task: Task, key: number) => {
+            return <TaskItem key={key} task={task} onComplete={handleTaskStatus}/>
+          })}
+        </div>
+        <div className='done'>
+          <h1>Completas</h1>
+          {taskList.filter(isCompleted).map( (task: Task, key: number) => {
+            return <TaskItem key={key} task={task} onComplete={handleTaskStatus}/>
+          })}
+        </div>
       </div>
     </div>
   );
