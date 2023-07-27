64909ea9188c46d159cce867df086d6f1f3a34e3
matheusberger <matheuscberger@gmail.com>
2023-07-27 15:35:58 -0300

    my-app/src/components/TaskItem.css

--- my-app/src/components/TaskItem.tsx
+++ my-app/src/components/TaskItem.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Task } from '../interfaces/Task';
 import './TaskItem.css';
 
 interface Props {
     task: Task;
+    onComplete: (task:Task) => void
 }
 
-const TaskItem = ({ task }: Props) => {
+const TaskItem = ({ task, onComplete }: Props) => {
+
+    const [isChecked, setIsChecked] = useState(task.completed);
+    const handleOnChange = () => {
+        setIsChecked(!isChecked);
+        onComplete(task)
+    };
+
     return (
         <div className="TaskItem">
-            <button>X</button>
+            <input 
+            type="checkbox" 
+            id={task.name} 
+            name="completedCheckbox" 
+            checked={isChecked}
+            onChange={handleOnChange}/>
             <span>{task.name}</span>
         </div>
     );
